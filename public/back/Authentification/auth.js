f94a35de66c9826915e5d5eb3c20b2ba312b0170
Ahmed <ahmed.hamdaoui@tech-instinct.com>
2020-06-17 17:31:10 +0100

    fix authetication

--- public/back/Authentification/auth.js
+++ public/back/Authentification/auth.js
@@ -1,42 +1,47 @@
 
-(function () { 
-var config = {
-    apiKey: "AIzaSyDEW9kA0lZ9KWmVNsqCGOkJw28TM6AqWTQ",
-    authDomain: "the-strokes-j.firebaseapp.com",
-    databaseURL: "https://the-strokes-j.firebaseio.com",
-    projectId: "the-strokes-j",
-    storageBucket: "the-strokes-j.appspot.com",
-    messagingSenderId: "537977635064",
-    appId: "1:537977635064:web:7ae688d622df62aeb8fb0d",
-    measurementId: "G-31ZT06QWHH"
-    // ....
-  }; 
+(function () {
+    var config = {
+        apiKey: "AIzaSyDEW9kA0lZ9KWmVNsqCGOkJw28TM6AqWTQ",
+        authDomain: "the-strokes-j.firebaseapp.com",
+        databaseURL: "https://the-strokes-j.firebaseio.com",
+        projectId: "the-strokes-j",
+        storageBucket: "the-strokes-j.appspot.com",
+        messagingSenderId: "537977635064",
+        appId: "1:537977635064:web:7ae688d622df62aeb8fb0d",
+        measurementId: "G-31ZT06QWHH"
+        // ....
+    };
 
-  firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
 
 
-/*recuperer les donner - get elem*/
+    /*recuperer les donner - get elem*/
 
-const txtEmail = document.getElementById("textEmail") ; 
-const txtPassword = document.getElementById('textPassword')
-const btnLogin= document.getElementById('btnLogin')
+    const txtEmail = document.getElementById("textEmail");
+    const txtPassword = document.getElementById('textPassword')
+    const btnLogin = document.getElementById('btnLogin')
 
-/* add login event */
+    /* add login event */
 
-btnLogin.addEventListener('click', e=> {
+    btnLogin.addEventListener('click', e => {
 
-    /*get email and pass */
+        /*get email and pass */
+        e.preventDefault();
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
 
-    const email = txtEmail.value ; 
-    const pass = txtPassword.value ; 
-    const auth =  firebase.auth() ; 
+        // sign in */
 
-    // sign in */
-
-    const promise = auth.signInWithEmailAndPassword(email , pass) ; 
-    promise.catch(e => console.log(e.message)) ; 
-}) ; 
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.then((res) => {
+            console.log(res);
+        })
+        promise.catch(e => {
+            console.log(e.message)
+        });
+    });
 
 
 }()); 
